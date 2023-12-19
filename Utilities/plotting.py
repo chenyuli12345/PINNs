3e8ded3b4f523a781d52f70e779578919c04e3e9
LI Chenyu <153217294+chenyuli12345@users.noreply.github.com>
2023-12-20 00:19:22 +0800

    更新plotting

--- Utilities/plotting.py
+++ Utilities/plotting.py
@@ -32,34 +32,34 @@ def figsize(scale, nplots = 1):
     #创建了一个由前面计算的图形宽度和高度构成的列表fig_size，并返回这个列表。
     return fig_size
 
-# #这段代码是在配置 matplotlib 以使用LaTeX输出图形。这是通过创建一个名为pgf_with_latex 的字典来实现的，这个字典包含了一系列的配置项。
-# pgf_with_latex = {                      # setup matplotlib to use latex for output
-#     #设置了LaTeX的编译系统为pdflatex。如果你使用的是 xetex 或 lualatex，你需要修改这个设置。
-#     "pgf.texsystem": "pdflatex",        # change this if using xetex or lautex
-#     #设置matplotlib使用LaTeX来写所有的文本。
-#     "text.usetex": True,                # use LaTeX to write all text
-#     #设置默认的字体族为serif（衬线字体）。
-#     "font.family": "serif",
-#     #设置各种字体的列表。这些列表为空，意味着使用matplotlib生成图形的Python脚本所在的LaTeX文档中继承字体设置。也就是说，matplotlib将使用LaTeX文档中定义的字体设置来渲染图形中的文本。这样可以确保图形的字体与LaTeX文档的字体保持一致，使得图形与文档整体风格一致。。
-#     "font.serif": [],                   # blank entries should cause plots to inherit fonts from the document
-#     "font.sans-serif": [],
-#     "font.monospace": [],
-#     #这几行在设置 matplotlib 图形的各种字体大小。这些设置是通过修改 matplotlib 的配置参数（rcParams）来实现的。
-#     "axes.labelsize": 10,               # LaTeX default is 10pt font.坐标轴标签的字体大小为 10 点。这是 LaTeX 的默认字体大小
-#     "font.size": 10,                    # 图形中其他文本的默认字体大小为 10 点
-#     "legend.fontsize": 8,               # Make the legend/label fonts a little smaller,图例的字体大小为 8 点。这是为了让图例的字体稍微小一些，以便不会干扰到图形的主要内容
-#     "xtick.labelsize": 8,               #这两行分别设置了 x 轴和 y 轴刻度标签的字体大小为 8 点
-#     "ytick.labelsize": 8,            
-#     "figure.figsize": figsize(1.0),     # default fig size of 0.9 textwidth,设置了图形的默认大小为文本宽度的 1.0 倍。这里使用了前面定义的 figsize 函数来计算图形的大小
-#     #这段代码是在设置 matplotlib 生成图形时使用的 LaTeX 预设命令。这些命令被放在 "pgf.preamble" 的列表中。这些预设命令将在 matplotlib 生成图形时被使用。这意味着你可以在图形的文本中使用 LaTeX 的功能，例如数学公式、特殊字符等等。
-#     "pgf.preamble": [
-#         r"\usepackage[utf8x]{inputenc}",    # use utf8 fonts becasue your computer can handle it :)导入了inputenc宏包，并设置了其选项为 utf8x。inputenc宏包用于设置LaTeX文档的输入编码，utf8x选项表示使用UTF-8编码。这意味着你可以在图形的文本中直接使用 UTF-8 编码的字符，例如中文、日文、希腊字母等等。
-#         r"\usepackage[T1]{fontenc}",        # plots will be generated using this preamble,导入了fontenc宏包，并设置了其选项为 T1。fontenc 宏包用于设置LaTeX文档的字体编码，T1选项表示使用T1编码。T1编码支持西欧语言的大多数字符，包括一些特殊的字符，如法语的重音字符等。
-#         ]
-#     }
-# mpl.rcParams.update(pgf_with_latex) #将这些设置应用到 matplotlib。rcParams 是 matplotlib 的配置对象，update 方法可以用来修改这些配置。
+#这段代码是在配置 matplotlib 以使用LaTeX输出图形。这是通过创建一个名为pgf_with_latex 的字典来实现的，这个字典包含了一系列的配置项。
+pgf_with_latex = {                      # setup matplotlib to use latex for output
+    #设置了LaTeX的编译系统为pdflatex。如果你使用的是 xetex 或 lualatex，你需要修改这个设置。
+    "pgf.texsystem": "pdflatex",        # change this if using xetex or lautex
+    #设置matplotlib使用LaTeX来写所有的文本。
+    "text.usetex": True,                # use LaTeX to write all text
+    #设置默认的字体族为serif（衬线字体）。
+    "font.family": "serif",
+    #设置各种字体的列表。这些列表为空，意味着使用matplotlib生成图形的Python脚本所在的LaTeX文档中继承字体设置。也就是说，matplotlib将使用LaTeX文档中定义的字体设置来渲染图形中的文本。这样可以确保图形的字体与LaTeX文档的字体保持一致，使得图形与文档整体风格一致。。
+    "font.serif": [],                   # blank entries should cause plots to inherit fonts from the document
+    "font.sans-serif": [],
+    "font.monospace": [],
+    #这几行在设置 matplotlib 图形的各种字体大小。这些设置是通过修改 matplotlib 的配置参数（rcParams）来实现的。
+    "axes.labelsize": 10,               # LaTeX default is 10pt font.坐标轴标签的字体大小为 10 点。这是 LaTeX 的默认字体大小
+    "font.size": 10,                    # 图形中其他文本的默认字体大小为 10 点
+    "legend.fontsize": 8,               # Make the legend/label fonts a little smaller,图例的字体大小为 8 点。这是为了让图例的字体稍微小一些，以便不会干扰到图形的主要内容
+    "xtick.labelsize": 8,               #这两行分别设置了 x 轴和 y 轴刻度标签的字体大小为 8 点
+    "ytick.labelsize": 8,            
+    "figure.figsize": figsize(1.0),     # default fig size of 0.9 textwidth,设置了图形的默认大小为文本宽度的 1.0 倍。这里使用了前面定义的 figsize 函数来计算图形的大小
+    #这段代码是在设置 matplotlib 生成图形时使用的 LaTeX 预设命令。这些命令被放在 "pgf.preamble" 的列表中。这些预设命令将在 matplotlib 生成图形时被使用。这意味着你可以在图形的文本中使用 LaTeX 的功能，例如数学公式、特殊字符等等。
+    "pgf.preamble": [
+        r"\usepackage[utf8x]{inputenc}",    # use utf8 fonts becasue your computer can handle it :)导入了inputenc宏包，并设置了其选项为 utf8x。inputenc宏包用于设置LaTeX文档的输入编码，utf8x选项表示使用UTF-8编码。这意味着你可以在图形的文本中直接使用 UTF-8 编码的字符，例如中文、日文、希腊字母等等。
+        r"\usepackage[T1]{fontenc}",        # plots will be generated using this preamble,导入了fontenc宏包，并设置了其选项为 T1。fontenc 宏包用于设置LaTeX文档的字体编码，T1选项表示使用T1编码。T1编码支持西欧语言的大多数字符，包括一些特殊的字符，如法语的重音字符等。
+        ]
+    }
+mpl.rcParams.update(pgf_with_latex) #将这些设置应用到 matplotlib。rcParams 是 matplotlib 的配置对象，update 方法可以用来修改这些配置。
 
-# import matplotlib.pyplot as plt #pyplot 是 matplotlib 库的一个子模块，它提供了一种简单的方式来创建图表。pyplot 提供了大量的函数，用于创建各种图表，包括线图、柱状图、散点图、饼图等。这些函数大多数都提供了丰富的参数，可以用来定制图表的各个方面，如颜色、线型、标签等。其设计风格类似于 MATLAB，它的函数通常会改变当前的图或者子图，这使得它非常适合用于交互式环境，如 Jupyter notebook。
+import matplotlib.pyplot as plt #pyplot 是 matplotlib 库的一个子模块，它提供了一种简单的方式来创建图表。pyplot 提供了大量的函数，用于创建各种图表，包括线图、柱状图、散点图、饼图等。这些函数大多数都提供了丰富的参数，可以用来定制图表的各个方面，如颜色、线型、标签等。其设计风格类似于 MATLAB，它的函数通常会改变当前的图或者子图，这使得它非常适合用于交互式环境，如 Jupyter notebook。
 
 # I make my own newfig and savefig functions
 #这段代码定义了一个名为newfig的函数，该函数用于创建一个新的图形（figure）和子图（axes）。这个函数接受两个参数：width和nplots。width 用于指定图形的宽度，nplots 用于指定子图的数量，默认为 1。最后，函数返回了创建的图形和子图。这样，用户就可以在这个图形和子图上进行绘图操作。
@@ -86,25 +86,25 @@ def savefig(filename, crop = True):
         plt.savefig('{}.eps'.format(filename))
 
 
-# # Simple plot,一段简单的示例，用于绘制指数移动平均（EMA）图
-# fig, ax  = newfig(1.0)
+# Simple plot,一段简单的示例，用于绘制指数移动平均（EMA）图
+fig, ax  = newfig(1.0)
 
-# def ema(y, a):  #定义了一个名为ema的函数用于计算指数移动平均。接受两个参数：y是一个包含数据的列表，a是平滑因子。
-#    s = []
-#    s.append(y[0])  #函数首先将y的第一个元素添加到结果列表s中
-#    for t in range(1, len(y)):
-#        s.append(a*y[t]+(1-a)*s[t-1]) #对y的剩余元素进行迭代，每次迭代都计算，并将结果添加到s中
-#    return np.array(s) #最后，函数返回s的numpy数组形式
+def ema(y, a):  #定义了一个名为ema的函数用于计算指数移动平均。接受两个参数：y是一个包含数据的列表，a是平滑因子。
+   s = []
+   s.append(y[0])  #函数首先将y的第一个元素添加到结果列表s中
+   for t in range(1, len(y)):
+       s.append(a*y[t]+(1-a)*s[t-1]) #对y的剩余元素进行迭代，每次迭代都计算，并将结果添加到s中
+   return np.array(s) #最后，函数返回s的numpy数组形式
    
-# y = [0]*200 #创建了一个名为y的列表，该列表前200个元素为0
-# #这行代码使用了列表的`extend`方法。用于将一个列表（或任何可迭代对象）的所有元素添加到当前列表的末尾。
-# #这里`[20]*(1000-len(y))` 创建了一个新的列表，这个列表的长度为 `1000-len(y)`，所有元素都是 `20`。然后，这个新列表的所有元素都被添加到 `y` 的末尾
-# y.extend([20]*(1000-len(y)))  #将y的后800个元素设置为20
-# s = ema(y, 0.01) #使用上面定义的ema函数来计算y的指数移动平均，平滑因子为0.01
+y = [0]*200 #创建了一个名为y的列表，该列表前200个元素为0
+#这行代码使用了列表的`extend`方法。用于将一个列表（或任何可迭代对象）的所有元素添加到当前列表的末尾。
+#这里`[20]*(1000-len(y))` 创建了一个新的列表，这个列表的长度为 `1000-len(y)`，所有元素都是 `20`。然后，这个新列表的所有元素都被添加到 `y` 的末尾
+y.extend([20]*(1000-len(y)))  #将y的后800个元素设置为20
+s = ema(y, 0.01) #使用上面定义的ema函数来计算y的指数移动平均，平滑因子为0.01
 
-# #下面的代码中，`ax` 是一个 `AxesSubplot` 对象，之前它是通过调用`fig.add_subplot(111)` 创建的。`AxesSubplot` 对象代表了图形中的一个子图。
-# ax.plot(s) # ax.plot(s) 在子图ax上绘制一个线图，线图的数据来源于's'。然后，使用 ax.set_xlabel('X Label') 和 ax.set_ylabel('EMA') 设置了 x 轴和 y 轴的标签
-# ax.set_xlabel('X Label') #设置子图ax的x轴标签为'X Label'
-# ax.set_ylabel('EMA') #设置子图ax的y轴标签为'EMA'
+#下面的代码中，`ax` 是一个 `AxesSubplot` 对象，之前它是通过调用`fig.add_subplot(111)` 创建的。`AxesSubplot` 对象代表了图形中的一个子图。
+ax.plot(s) # ax.plot(s) 在子图ax上绘制一个线图，线图的数据来源于's'。然后，使用 ax.set_xlabel('X Label') 和 ax.set_ylabel('EMA') 设置了 x 轴和 y 轴的标签
+ax.set_xlabel('X Label') #设置子图ax的x轴标签为'X Label'
+ax.set_ylabel('EMA') #设置子图ax的y轴标签为'EMA'
 
-# savefig('ema') #将图形保存为文件，文件名为'ema'，默认情况下，文件会被保存在当前的工作目录
+savefig('ema') #将图形保存为文件，文件名为'ema'，默认情况下，文件会被保存在当前的工作目录
